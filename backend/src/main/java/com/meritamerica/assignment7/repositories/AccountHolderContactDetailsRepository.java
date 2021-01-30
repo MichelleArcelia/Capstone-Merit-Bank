2ad98599978d6bcb281662550e6e69e2d74d5509
Jameson <jamesonsquyres@gmail.com>
2021-01-30 15:53:33 -0600

    refactored and cleaned

--- backend/src/main/java/com/meritamerica/assignment7/repositories/AccountHolderContactDetailsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.meritamerica.assignment7.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.meritamerica.assignment7.models.AccountHolderContactDetails;
-
-public interface AccountHolderContactDetailsRepository extends JpaRepository<AccountHolderContactDetails, Integer> {
-	AccountHolderContactDetails findByAccountHolder(int id);
-}
