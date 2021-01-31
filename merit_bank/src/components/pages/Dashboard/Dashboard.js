bef160d6a031c6abf680ae0153dba3fffa8843b2
Michelle Neptune <michellea.neptune@gmail.com>
2021-01-31 13:37:31 -0600

    DashboardChanages

--- merit_bank/src/components/pages/Dashboard/Dashboard.js
+++ merit_bank/src/components/pages/Dashboard/Dashboard.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import HeroSection from '../../HeroSection'
 //pulling data properties 
-import { homeObjOne } from './Data'
+//import { homeObjOne } from './Data'
 import Pricing from '../../Pricing';
 
 function Dashboard() {
     return (
         <>
             <Pricing />
-            <HeroSection { ...homeObjOne} />
+            
         </>
     )
 }
